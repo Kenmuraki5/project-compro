e563547b921685450af318aa0c0be02a37586bd2
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-04-28 15:26:25 +0700

    update

--- game.c
+++ game.c
@@ -4,7 +4,25 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-void help();
+void help()
+{
+    system("cls");
+    printf("\n\n                              HELP");
+    printf("\n *************************************************************************");
+    printf("\n                           C program Quiz Game");
+    printf("\n 1) There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n    In warmup round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n 2) Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded $100,000.");
+    printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
+    printf("\n 3> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n 4> You will be asked questions continuously if you keep giving the right answers.");
+	printf("\n\n\t*****************************************************************");
+	printf("\n\n\t*****This quizgame is part of the project of computer programming with c code ********");
+}
 int main()
 {
     int countr,number,number2,count,i,n;
@@ -179,25 +197,4 @@ int main()
 		}
 	}
 }
-    void help()
-   
-    {
-        system("cls");
-        printf("\n\n                              HELP");
-        printf("\n -------------------------------------------------------------------------");
-        printf("\n ......................... C program Quiz Game...........");
-        printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
-        printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
-        printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
-        printf("\n    right answers otherwise you can't play the Game...........");
-        printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
-        printf("\n    total 10 questions each right answer will be awarded $100,000.");
-        printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
-        printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
-        printf("\n    right option");
-        printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
-        printf("\n >> No negative marking for wrong answers");
-
-	    printf("\n\n\t*********************BEST OF LUCK*********************************");
-	    printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by CODE WITH C TEAM********");}
 
