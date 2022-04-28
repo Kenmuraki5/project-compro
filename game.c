9798957ee3bf89db5aea62076a84f9508fd67e60
64070131_Jeerachaya Chareonphol <88493586+IceSac@users.noreply.github.com>
2022-04-28 15:08:51 +0700

    help fn

--- game.c
+++ game.c
@@ -4,6 +4,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+void help();
 int main()
 {
     int countr,number,number2,count,i,n;
@@ -46,6 +47,9 @@ int main()
     }
     else if (choice=='Q')
 		exit(1);
+    else if (choice=='H'){
+        help();
+        getch();}
     else
         goto mainhome;
     printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
@@ -175,3 +179,25 @@ int main()
 		}
 	}
 }
+    void help()
+   
+    {
+        system("cls");
+        printf("\n\n                              HELP");
+        printf("\n -------------------------------------------------------------------------");
+        printf("\n ......................... C program Quiz Game...........");
+        printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+        printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
+        printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+        printf("\n    right answers otherwise you can't play the Game...........");
+        printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+        printf("\n    total 10 questions each right answer will be awarded $100,000.");
+        printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
+        printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+        printf("\n    right option");
+        printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+        printf("\n >> No negative marking for wrong answers");
+
+	    printf("\n\n\t*********************BEST OF LUCK*********************************");
+	    printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by CODE WITH C TEAM********");}
+
